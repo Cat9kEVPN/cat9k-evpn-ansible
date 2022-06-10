00946192c1f32fa3650d35839ed9159faadf6b28
dvishchu <dvishchu@cisco.com>
2022-06-10 12:40:44 +0200

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Cat9k EVPN Ansible documentation'
+project = 'Cat9k EVPN Ansible'
 copyright = '2022, Cat9k EVPN Team'
 author = 'Cat9k EVPN Team'
 
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
